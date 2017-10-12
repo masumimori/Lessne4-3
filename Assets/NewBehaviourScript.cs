05b0d696dc6269256b63e615f3ccabb73dcb54c0
盛　ます美 <shinonome_77@wine.plala.or.jp>
2017-10-12 18:40:48 +0900

    no message

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    // Use this for initialization
+    // 要素数5の配列を初期化する
+    int[] array = new int[5];
+
+    // 配列の各要素に値を代入する
+
+   　　 array[0] = 100;
+        array[1] = 200;
+        array[2] = 500;
+        array[3] = 700;
+        array[4] = 900;
+
+        // 5処理を繰り返す 関数の初期値は5から　5からインクリメントで１個つつ減る
+
+               for (int i = 5; i <= 5; i--) {
+                        
+                }
+        
+        // 配列表示
+
+                Debug.Log(array[i]);
+        }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/boss.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    // 攻撃用の関数 
+    public void Magic()
+    {
+        private int mp = 53; //初期値53
+
+        // 10回攻撃する繰り返し
+        for (int i = 0; i < 10; i++)
+        {
+        if (mp <= 53){
+        // MPが53以下の場合攻撃できるので
+            public void Ac(int damage) { 
+   // 残りＭＰを減らしての表示
+
+        this.mp -= damage;
+        Debug.Log("魔法攻撃をした。残りMPは"damage + );｝
+
+  // 繰り返し文で上に当てはまらない場合
+  else {
+        Debug.Log("MPが足りないため魔法が使えない。);｝
+}
+        
+void Start ()
+    {
+    // 実行関数　１０回繰りかえして、メッセージあり
+    Boss lastboss = new Boss();
+}
+
+// Update is called once per frame
+void Update ()
+    {
+		
+	}
+}
