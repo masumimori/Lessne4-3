a01bf18440a5cf5a12f805a3579ae0d4eb9f7fc6
盛　ます美 <shinonome_77@wine.plala.or.jp>
2017-10-16 09:06:20 +0900

    二回目　ＢＯＳＳのプログラム

--- Assets/boss.cs
+++ Assets/boss.cs
@@ -4,31 +4,27 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // 攻撃用の関数 
+	private int mp = 53; //初期値53
+
+	// 攻撃用の関数 
     public void Magic()
     {
-        private int mp = 53; //初期値53
-
-        // 10回攻撃する繰り返し
-        for (int i = 0; i < 10; i++)
-        {
-        if (mp <= 53){
-        // MPが53以下の場合攻撃できるので
-            public void Ac(int damage) { 
-   // 残りＭＰを減らしての表示
-
-        this.mp -= damage;
-        Debug.Log("魔法攻撃をした。残りMPは"damage + );｝
+       if (mp <= 53){
+       this.mp -= damage;
+				Debug.Log("魔法攻撃をした。残りMPは"+damage);
 
   // 繰り返し文で上に当てはまらない場合
   else {
-        Debug.Log("MPが足りないため魔法が使えない。);｝
-}
-        
-void Start ()
+		Debug.Log("MPが足りないため魔法が使えない。");
+
+
+ void Start ()
     {
+	// 10回攻撃する繰り返し
+      for (int i = 0; i < 10; i++)
     // 実行関数　１０回繰りかえして、メッセージあり
     Boss lastboss = new Boss();
+	lastboss.Magic(5);
 }
 
 // Update is called once per frame
