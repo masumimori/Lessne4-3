59a9c4d8b2ac8d988f484c7e96032f2b42c8c2d8
盛　ます美 <shinonome_77@wine.plala.or.jp>
2017-10-25 20:46:55 +0900

    再提出　２

--- Assets/boss.cs
+++ Assets/boss.cs
@@ -7,16 +7,17 @@ public class Boss : MonoBehaviour
 	private int mp = 53; //初期値53
 
 	// 攻撃用の関数 
-	public void Magic(mp)
+	public void Magic(int reMp)
     {
-       if (mp <= 53){
-       this.mp -= damage;
-				Debug.Log("魔法攻撃をした。残りMPは"+damage);
+       if (mp >= reMp){
+			this.mp -= reMp;
+				Debug.Log("魔法攻撃をした。残りMPは"+mp);
 		}
   // 繰り返し文で上に当てはまらない場合
   else {
 Debug.Log("MPが足りないため魔法が使えない。");
 		}
+	}
 }
 public class Test : MonoBehaviour
 	void start()
